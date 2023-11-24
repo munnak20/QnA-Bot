230574761ad05b48309a3e10da1dacefa3ac46a2
munnak20 <105987153+munnak20@users.noreply.github.com>
2023-11-24 22:20:10 +0530

    Update queries.py

--- queries.py
+++ queries.py
@@ -3,7 +3,9 @@ from QnA import qna
 
 def outputQuery():
     while(True):
-        query= input("Please enter your query: ")
+        query= input("Please enter your query or to quit type q: ")
+        if query=="q":
+            break;
         output = qna(query)
         print(f"AI: {str(output.text)}")
         print("########################")
