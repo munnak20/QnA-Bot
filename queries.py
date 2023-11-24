fb68e71f8ec1ae90d57761e633353d9b15b020d0
Munna Kumar <munnak20@iitk.ac.in>
2023-11-24 22:13:07 +0530

    upaded queries

--- queries.py
+++ queries.py
@@ -1,20 +1,11 @@
 from QnA import qna
 
 
-def outputQuery(query):
-    print("########################")
-    print(f"User: \"{query}\"")
-    output = qna(query)
-    print(f"AI: {str(output.text)}")
-    print("########################")
-    print()
-
-query1 = "Which product contains garlic oil?"
-
-query2 = "Tell me something about Pudina Chutney Masala"
-
-query3 = "Tell me the best perfume for men "
-
-outputQuery(query1)
-outputQuery(query2)
-outputQuery(query3)
+def outputQuery():
+    while(True):
+        query= input("Please enter your query: ")
+        output = qna(query)
+        print(f"AI: {str(output.text)}")
+        print("########################")
+        print()
+outputQuery()
